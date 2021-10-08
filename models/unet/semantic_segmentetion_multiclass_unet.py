8e07e066dcfbdc742000c5187a7211f63996548b
Aynur19 <nasybullin.aynur@yandex.ru>
2021-10-08 15:03:09 +0300

    add model

---
+++ models/unet/semantic_segmentetion_multiclass_unet.py
@@ -0,0 +1,183 @@
+import numpy as np
+import pandas as pd
+
+import cv2
+import matplotlib.pyplot as plt
+import os
+import tensorflow as tf
+from tensorflow.keras.utils import Sequence
+
+from keras.models import Model
+from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, concatenate, Conv2DTranspose, BatchNormalization, Dropout, Lambda
+
+from PIL import Image
+import io
+
+
+
+num_classes = 23
+H = 800
+W = 1200
+
+
+# чтение файлов
+def read_image(x):
+    x = cv2.imread(x, cv2.IMREAD_COLOR)
+    x = cv2.resize(x, (W, H))
+    x = x/255.0
+    x = x.astype(np.float32)
+    return x
+
+
+def read_mask(x):
+    x = cv2.imread(x, cv2.IMREAD_GRAYSCALE)
+    x = cv2.resize(x, (W, H))
+    x = x.astype(np.int32)
+    return x
+
+
+# подготовка изображений для обучения
+def tf_dataset(x,y, batch=4):
+    dataset = tf.data.Dataset.from_tensor_slices((x,y)) # Dataset object from Tensorflow
+    dataset = dataset.shuffle(buffer_size=100) 
+    dataset = dataset.map(preprocess) # Applying preprocessing to every batch in the Dataset object
+    dataset = dataset.batch(batch) # Determine atch-size
+    dataset = dataset.repeat()
+    dataset = dataset.prefetch(2) # Optimization
+    return dataset
+        
+
+def preprocess(x,y):
+    def f(x,y):
+        x = x.decode()
+        y = y.decode()
+        image = read_image(x)
+        mask = read_mask(y)
+        return image, mask
+    
+    image, mask = tf.numpy_function(f,[x,y],[tf.float32, tf.int32])
+    mask = tf.one_hot(mask, num_classes, dtype=tf.int32)
+    image.set_shape([H, W, 3])    # In the Images, number of channels = 3. 
+    mask.set_shape([H, W, num_classes])    # In the Masks, number of channels = number of classes. 
+    return image, mask
+
+
+# подготовка изображений для теста
+def test_dataset(x, batch=4):
+    dataset = tf.data.Dataset.from_tensor_slices(x)
+    dataset = dataset.map(preprocess_test)
+    dataset = dataset.batch(batch)
+    dataset = dataset.prefetch(2)
+    return dataset
+        
+
+def preprocess_test(x):
+    def f(x):
+        x = x.decode()
+        image = read_image(x)
+        return image
+    
+    image = tf.convert_to_tensor(tf.numpy_function(f, [x] , [tf.float32]))
+    image = tf.reshape(image, (H, W, 3))    # In the Images, number of channels = 3.  
+    return image
+
+
+# модель нейронной сети
+def multi_unet_model(n_classes=23, IMG_HEIGHT=H, IMG_WIDTH=W, IMG_CHANNELS=3):
+    #Build the model
+    inputs = Input((IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
+    s = inputs
+
+    #Contraction path
+    c1 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(s)
+    c1 = Dropout(0.1)(c1)  # Original 0.1
+    c1 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c1)
+    p1 = MaxPooling2D((2, 2))(c1)
+    
+    c2 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p1)
+    c2 = Dropout(0.1)(c2)  # Original 0.1
+    c2 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c2)
+    p2 = MaxPooling2D((2, 2))(c2)
+     
+    c3 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p2)
+    c3 = Dropout(0.1)(c3)
+    c3 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c3)
+    p3 = MaxPooling2D((2, 2))(c3)
+     
+    c4 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p3)
+    c4 = Dropout(0.1)(c4)
+    c4 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c4)
+    p4 = MaxPooling2D(pool_size=(2, 2))(c4)
+     
+    c5 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p4)
+    c5 = Dropout(0.3)(c5)
+    c5 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c5)
+    
+    #Expansive path 
+    u6 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(c5)
+    u6 = concatenate([u6, c4])
+    c6 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u6)
+    c6 = Dropout(0.1)(c6)
+    c6 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c6)
+     
+    u7 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(c6)
+    u7 = concatenate([u7, c3])
+    c7 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u7)
+    c7 = Dropout(0.2)(c7)
+    c7 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c7)
+     
+    u8 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(c7)
+    u8 = concatenate([u8, c2])
+    c8 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u8)
+    c8 = Dropout(0.1)(c8)  # Original 0.1
+    c8 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c8)
+     
+    u9 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same')(c8)
+    u9 = concatenate([u9, c1], axis=3)
+    c9 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u9)
+    c9 = Dropout(0.1)(c9)  # Original 0.1
+    c9 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c9)
+     
+    outputs = Conv2D(n_classes, (1, 1), activation='softmax')(c9)
+     
+    model = Model(inputs=[inputs], outputs=[outputs])
+    
+    #NOTE: Compile the model in the main program to make it easy to test with various loss functions
+    model.compile(optimizer='adam', loss=['categorical_crossentropy'], metrics=['accuracy'])
+    
+    # model.summary()
+    return model
+
+model = multi_unet_model()
+
+# получаем модель НН
+model =  tf.keras.models.load_model('/content/multi_unet_model_e350_v5.h5')
+
+# директория откуда будет браться изображение
+images_dir = '/content/alferevo-drone-dataset'
+images_name = list(map(lambda x: x.replace('.jpg', ''), os.listdir(images_dir)))
+
+# путь к тестируемому изображению
+img_test = [os.path.join(images_dir, f"{name}") for name in images_name]
+
+# путь к словарю "метка-RGB"
+color_dict = pd.read_csv('/content/class_dict.csv')
+
+# путь к директории куда отправится RGB семантическая сегментация
+rgb_dir = '/content/rgb_result/'
+
+def predict_images(model, img_test, color_dict, rgb_dir, ):
+    
+    cmap = np.array(list(color_dict[[' r', ' g', ' b']].transpose().to_dict('list').values()))
+    
+    pred = model.predict(test_dataset(img_test, batch=1), steps=100)
+    predictions = np.argmax(pred, axis=3)
+    predictions = predictions.flatten()
+    predictions = predictions.reshape(-1, 800, 1200)
+
+    #img 
+    #for i in range(predictions.shape[0]):
+    img = Image.fromarray(np.asarray(cmap[predictions[0]]).astype(np.uint8))
+        #img.save(f'{rgb_dir}{images_name[i][:-4]}_RGB.png')
+        
+    return
